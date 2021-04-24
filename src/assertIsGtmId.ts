1ab2b4b9055ec7ed1a8bf91551854d79d9be771b
Shinigami92 <chrissi92@hotmail.de>
2021-04-24 13:55:13 +0200

    Format code

--- src/assertIsGtmId.ts
+++ src/assertIsGtmId.ts
@@ -8,7 +8,7 @@ const GTM_ID_PATTERN: RegExp = /^GTM-[0-9A-Z]+$/;
  * @param id A GTM Container ID.
  */
 export function assertIsGtmId(id: string): asserts id {
-  if (typeof id !== "string" || !GTM_ID_PATTERN.test(id)) {
+  if (typeof id !== 'string' || !GTM_ID_PATTERN.test(id)) {
     throw new Error(`GTM-ID '${id}' is not valid`);
   }
 }
