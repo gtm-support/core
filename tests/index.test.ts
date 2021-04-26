aa5f08d837c463d23e71cf8ac21b4a8855ea2b30
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2021-04-26 15:39:39 +0200

    Add test to check mandatory exports

---
+++ tests/index.test.ts
@@ -0,0 +1,11 @@
+import { assertIsGtmId, GtmSupport, GTM_ID_PATTERN, hasScript, loadScript } from '../src/index';
+
+describe('index mandatory exports', () => {
+  test('should export', () => {
+    expect(GtmSupport).toBeInstanceOf(Object);
+    expect(GTM_ID_PATTERN).toBeInstanceOf(RegExp);
+    expect(assertIsGtmId).toBeInstanceOf(Function);
+    expect(loadScript).toBeInstanceOf(Function);
+    expect(hasScript).toBeInstanceOf(Function);
+  });
+});
