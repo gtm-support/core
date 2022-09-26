34d4eda7e376e82ffc744f24508959cd33aada84
Shinigami92 <chrissi92@hotmail.de>
2022-09-26 15:24:08 +0200

    Improve GTM-ID assertion error

--- src/assert-is-gtm-id.ts
+++ src/assert-is-gtm-id.ts
@@ -9,7 +9,14 @@ export const GTM_ID_PATTERN: RegExp = /^GTM-[0-9A-Z]+$/;
  * @param id A GTM Container ID.
  */
 export function assertIsGtmId(id: string): asserts id {
-  if (typeof id !== 'string' || !GTM_ID_PATTERN.test(id)) {
-    throw new Error(`GTM-ID '${id}' is not valid`);
+  if (typeof id !== 'string' || !GTM_ID_PATTERN.exec(id)) {
+    const suggestion: string = String(id)
+      .toUpperCase()
+      .replace(/.*-|[^0-9A-Z]/g, '');
+    const suggestionText: string =
+      suggestion.length === 0 ? '' : ` Did you mean 'GTM-${suggestion}'?`;
+    throw new Error(
+      `'${id}' is not a valid GTM-ID (${GTM_ID_PATTERN}).${suggestionText}`,
+    );
   }
 }
--- tests/assert-is-gtm-id.test.ts
+++ tests/assert-is-gtm-id.test.ts
@@ -4,25 +4,43 @@ import { assertIsGtmId } from '../src/index';
 describe('assert-is-gtm-id', () => {
   test('valid', () => {
     expect(() => assertIsGtmId('GTM-DEMO')).not.toThrow(
-      new Error("GTM-ID 'GTM-DEMO' is not valid"),
+      new Error("'GTM-DEMO' is not a valid GTM-ID (/^GTM-[0-9A-Z]+$/)."),
     );
   });
 
   test('invalid - empty string', () => {
     expect(() => assertIsGtmId('')).toThrow(
-      new Error("GTM-ID '' is not valid"),
+      new Error("'' is not a valid GTM-ID (/^GTM-[0-9A-Z]+$/)."),
     );
   });
 
   test('invalid - wrong prefix', () => {
     expect(() => assertIsGtmId('GA-DEMO')).toThrow(
-      new Error("GTM-ID 'GA-DEMO' is not valid"),
+      new Error(
+        "'GA-DEMO' is not a valid GTM-ID (/^GTM-[0-9A-Z]+$/). Did you mean 'GTM-DEMO'?",
+      ),
     );
   });
 
   test('invalid - non alphanumeric', () => {
     expect(() => assertIsGtmId('GTM-@')).toThrow(
-      new Error("GTM-ID 'GTM-@' is not valid"),
+      new Error("'GTM-@' is not a valid GTM-ID (/^GTM-[0-9A-Z]+$/)."),
+    );
+  });
+
+  test('invalid - wrong type', () => {
+    expect(() => assertIsGtmId(42 as unknown as string)).toThrow(
+      new Error(
+        "'42' is not a valid GTM-ID (/^GTM-[0-9A-Z]+$/). Did you mean 'GTM-42'?",
+      ),
+    );
+  });
+
+  test('invalid - passed nothing', () => {
+    expect(() => assertIsGtmId(undefined as unknown as string)).toThrow(
+      new Error(
+        "'undefined' is not a valid GTM-ID (/^GTM-[0-9A-Z]+$/). Did you mean 'GTM-UNDEFINED'?",
+      ),
     );
   });
 });
