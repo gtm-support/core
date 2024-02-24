04f6bbddd6cda665bd2a0862117892f4ccc16933
Eljas Jalo <eljasjal@gmail.com>
2024-02-24 19:13:46 +0200

    refactor: removes global declares for window object

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,6 @@ import type { DataLayerObject } from './data-layer-object';
 declare global {
   interface Window {
     /**
-     * Dynamic `dataLayer` key used by GTM.
-     *
-     * @see [developers.google.com/tag-manager/devguide/datalayer#rename_the_data_layer](https://developers.google.com/tag-platform/tag-manager/datalayer#rename_the_data_layer)
-     */
-    [key: string]: any;
-
-    /**
      * Default `dataLayer` used by GTM.
      *
      * @see [developers.google.com/tag-manager/devguide](https://developers.google.com/tag-manager/devguide)
