f5ee9503f13122bb87cabc1f2523700f300c904a
Fabian Poels <fabian@fabianpoels.com>
2024-05-22 18:21:39 +0200

    fix for newer GTM id's starting with 'G-'

--- src/assert-is-gtm-id.ts
+++ src/assert-is-gtm-id.ts
@@ -1,10 +1,10 @@
 /** GTM Container ID pattern. */
-export const GTM_ID_PATTERN: RegExp = /^GTM-[0-9A-Z]+$/;
+export const GTM_ID_PATTERN: RegExp = /^(GTM|G)-[0-9A-Z]+$/;
 
 /**
  * Assert that the given id is a valid GTM Container ID.
  *
- * Tested against pattern: `/^GTM-[0-9A-Z]+$/`.
+ * Tested against pattern: `/^(GTM|G)-[0-9A-Z]+$/`.
  *
  * @param id A GTM Container ID.
  */
@@ -14,7 +14,7 @@ export function assertIsGtmId(id: string): asserts id {
       .toUpperCase()
       .replace(/.*-|[^0-9A-Z]/g, '');
     const suggestionText: string =
-      suggestion.length === 0 ? '' : ` Did you mean 'GTM-${suggestion}'?`;
+      suggestion.length === 0 ? '' : ` Did you mean 'GTM-${suggestion}' or 'G-${suggestion}'?`;
     throw new Error(
       `'${id}' is not a valid GTM-ID (${GTM_ID_PATTERN}).${suggestionText}`,
     );
